87b3f3a18ac1a69e506245d88755d0bce9ffbcec
Juan Jimenez <jcjv86@gmail.com>
2023-03-26 18:19:58 +0200

    notebooks updated

--- app/menu.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
--- app/uploader.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-image = st.file_uploader("Please choose a .jpg file to check if there is a tumor or not",type='jpg')
